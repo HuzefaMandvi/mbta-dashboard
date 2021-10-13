617590435f4b4907d81e0f6a02047e29ecd17f52
Huzefa Mandviwala <hzmandviwala@gmail.com>
2021-10-13 13:47:01 -0400

    use the mbta api to get a route ID and display it from the predictions

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
+from os import readlink
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+# https://api-v3.mbta.com/predictions?filter[stop]=place-north&filter[route_type]=2&direction_id=0&sort=-departure_time
+
+api_url_base = "https://api-v3.mbta.com"
+api_url_predictions = api_url_base + "/predictions"
+api_url_schedule = api_url_base + "/schedules"
+params = {'filter[stop]':'place-north', 'filter[route_type]':'2', 'direction_id':'0', 'sort':'-departure_time'}
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    r = requests.get(url=api_url_predictions, params=params)
+    data = r.json()
+    return data["data"][0]["relationships"]["route"]["data"]["id"]

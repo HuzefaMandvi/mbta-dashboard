6ec20b2705cb6abf15248c663bb6eee4d86b3dd2
Huzefa Mandviwala <hzmandviwala@gmail.com>
2021-10-13 15:09:54 -0400

    show predicted departure times and statuses in a table

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from os import readlink
 from flask import Flask
+from flask_table import Table, Col
 import requests
 
 app = Flask(__name__)
@@ -11,8 +12,30 @@ api_url_predictions = api_url_base + "/predictions"
 api_url_schedule = api_url_base + "/schedules"
 params = {'filter[stop]':'place-north', 'filter[route_type]':'2', 'direction_id':'0', 'sort':'-departure_time'}
 
+class ItemTable(Table):
+    border = True
+    route_name = Col('Route')
+    departure_time = Col('Departure')
+    status = Col('Status')
+
+class Item(object):
+    def __init__(self, name, time, status):
+        self.route_name = name
+        self.departure_time = time
+        self.status = status
+
 @app.route("/")
 def hello_world():
+    items = []
+
     r = requests.get(url=api_url_predictions, params=params)
     data = r.json()
-    return data["data"][0]["relationships"]["route"]["data"]["id"]
+    for item in data["data"][0:2]:
+        route_name = item["relationships"]["route"]["data"]["id"]
+        route_departure_time = item["attributes"]["departure_time"]
+        route_status = item["attributes"]["status"]
+        route = Item(route_name, route_departure_time, route_status)
+        items.append(route)
+
+    table = ItemTable(items)
+    return table.__html__()

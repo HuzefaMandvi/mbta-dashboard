9559d889e45eef8916f541667c26645a3926e857
Huzefa Mandviwala <hzmandviwala@gmail.com>
2021-10-15 14:50:40 -0400

    convert timestamps to AM/PM time

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from os import readlink
 from flask import Flask
 from flask_table import Table, Col
@@ -18,24 +19,35 @@ class ItemTable(Table):
     departure_time = Col('Departure')
     status = Col('Status')
 
+#TODO - rename this class
 class Item(object):
     def __init__(self, name, time, status):
         self.route_name = name
         self.departure_time = time
         self.status = status
 
+def convert_timestamp_to_ampm(timestring):
+    # get rid of UTC offset
+    timestring = timestring[:-6]
+    # convert 24hr time to AM/PM time
+    d = datetime.strptime(timestring, "%Y-%m-%dT%H:%M:%S")
+    return d.strftime("%I:%M %p")
+
 @app.route("/")
 def hello_world():
     items = []
 
+    # first, try to get live predictions
     r = requests.get(url=api_url_predictions, params=params)
     data = r.json()
-    for item in data["data"][0:2]:
-        route_name = item["relationships"]["route"]["data"]["id"]
+    for item in data["data"][0:9]:
         route_departure_time = item["attributes"]["departure_time"]
-        route_status = item["attributes"]["status"]
-        route = Item(route_name, route_departure_time, route_status)
-        items.append(route)
+        if route_departure_time is not None:
+            route_name = item["relationships"]["route"]["data"]["id"]
+            route_departure_time = convert_timestamp_to_ampm(item["attributes"]["departure_time"])
+            route_status = item["attributes"]["status"]
+            route = Item(route_name, route_departure_time, route_status)
+            items.append(route)
 
     table = ItemTable(items)
     return table.__html__()
